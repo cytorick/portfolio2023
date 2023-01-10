4f59bcf9a25d67adfdacd770642bc1997050f25c
Rick Visser <rickvisser99@gmail.com>
2023-01-10 10:12:28 +0100

    Fixed an issue with images going over the nav

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -61,7 +61,7 @@
             @include('assets.alpine-tooltip')
             @stack('scripts-body-after')
 
-            <div class="btm-nav border-t border-green-600 md:hidden">
+            <div class="btm-nav border-t border-green-600 md:hidden z-50">
                 <a href="{{ route('home') }}" class="{{ (request()->is('/')) ? 'text-green-600 bg-gray-100 dark:bg-gray-900' : 'hover:text-green-600 text-gray-500 bg-gray-100 dark:bg-gray-900' }}">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                         <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
